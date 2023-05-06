f9203dbc4ae21dd82e1bdc459a6beb1524936abd
Meghann Stamper <meg.stamp@yahoo.com>
2023-05-06 14:50:25 -0400

    first commit

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+import functions from "firebase-functions"
+import express from "express"
+import cors from "cors"
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+export const api = functions.https.onRequest(app)
+
